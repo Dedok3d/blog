5e1a46ead1ecc1a79461d7c3cc763eac9c931dcd
Dedok3d <sersh_96@mail.ru>
2021-06-04 23:37:09 +0700

    add MillenniumFalcon to icon

--- src/component/logo/logo.tsx
+++ src/component/logo/logo.tsx
@@ -1,12 +1,17 @@
 import React from 'react';
 import styles from './logo.module.scss';
 
+import MillenniumFalcon from '../../../assets/millennium-falcon.svg';
+
 interface Props {
 }
 
 function Logo({ }: Props) {
 
-    return <div className={styles.logo}>Space news</div>;
+    return <div className={styles.logo}>
+        <MillenniumFalcon className={styles.svg} />
+        Space news
+    </div>;
 }
 
 export default Logo;
